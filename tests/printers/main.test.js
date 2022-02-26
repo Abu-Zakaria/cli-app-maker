7152f93953dc8c7b0b01dd02a1b10f05eb688d0a
Abu Zakaria <jakariablaine120@gmail.com>
2022-02-27 01:25:49 +0600

    Add more test for welcome message printer

--- tests/printers/main.test.js
+++ tests/printers/main.test.js
@@ -1,6 +1,7 @@
 const expect = require("chai").expect,
   should = require("chai").should();
 const { check } = require("./../helpers");
+const fs = require("fs");
 
 const WelcomePrinter = require("../../src/printers/welcome_message_printer");
 const welcome_printer = new WelcomePrinter();
@@ -13,4 +14,17 @@ it("should show error message when reading without any welcome text file", funct
       });
     });
   }, 100);
+});
+
+it("should show welcome message", function (done) {
+  fs.writeFileSync("./welcome.txt", "Welcome to cli app maker");
+  setTimeout(() => {
+    welcome_printer.read().then((data) => {
+      check(done, () => {
+        expect(data).to.include("Welcome to cli app maker");
+
+        fs.unlink("./welcome.txt", () => {});
+      });
+    });
+  });
 });
