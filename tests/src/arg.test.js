147e0423993ded050c3a82f084999132a71f024b
Abu Zakaria <jakariablaine120@gmail.com>
2022-03-02 22:32:34 +0600

    Add test when there are no argument value given

--- tests/src/arg.test.js
+++ tests/src/arg.test.js
@@ -15,4 +15,16 @@ describe("arg class testing", function() {
         expect(key).to.equals("asd")
         expect(val).to.equals("qwe")
     })
+
+    it("splits argument and return key(string) and value(null) when value haven't given", () => {
+        const input = "--qwe";
+
+        const arg = new Arg(input);
+
+        const key = arg.getKey();
+        const value = arg.getValue();
+
+        expect(key).to.equals("qwe")
+        expect(value).to.equals(null)
+    })
 })
